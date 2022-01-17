3dc41e2776bac8d9b9bd1b02680253cc32de0ec2
kananev <russrage@gmail.com>
2022-01-17 16:35:46 +0300

    add word-pattern.py

---
+++ easy/word-pattern.py
@@ -0,0 +1,9 @@
+def word_pattern(pattern: str, s: str) -> bool:
+    f = lambda x: map({}.setdefault, x, range(len(x)))
+    return list(f(pattern)) == list(f(s.split()))
+
+
+def test_word_pattern():
+    assert word_pattern('abba', 'dog cat cat dog')
+    assert not word_pattern('abba', 'dog cat cat fish')
+    assert not word_pattern('aaaa', 'dog cat cat dog')
