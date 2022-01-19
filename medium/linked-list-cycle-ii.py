901b52a52652e86a574e4f0d21e79f1b45abe12b
kananev <russrage@gmail.com>
2022-01-19 10:06:35 +0300

    add linked-list-cycle-ii

---
+++ medium/linked-list-cycle-ii.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def detect_cycle(head: Optional[ListNode]) -> Optional[ListNode]:
+    slow, fast = head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            slow = head
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+            return slow
+
+
+
